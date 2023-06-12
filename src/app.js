b524cae5be5e108509d23bd6a4edd834f0cd8ed3
Lado Asambadze <lado.asambadze1@gmail.com>
2023-06-12 14:43:11 +0400

    feat: get res for destination page

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ import express from "express";
 import connect from "./database/mongo.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
+import getDest from "./controllers/dest-controller.js";
+import swaggerMiddleware from "./middlewares/swg-midl.js";
 
 const app = express();
 dotenv.config();
 connect();
 app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/destinations/:name", getDest);
+app.use("/", ...swaggerMiddleware());
 
 app.listen(3000);
