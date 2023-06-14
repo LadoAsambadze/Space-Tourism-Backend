96664b680e481a4e0d7b4d4fa73a015e8d76d8fe
Lado Asambadze <lado.asambadze1@gmail.com>
2023-06-14 13:05:33 +0400

    test

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import getDest from "./controllers/dest-controller.js";
 import swaggerMiddleware from "./middlewares/swg-midl.js";
 import getCrew from "./controllers/crew-controller.js";
 import getTech from "./controllers/tech-controller.js";
-
+const port = process.env.PORT || 3000;
 const app = express();
 dotenv.config();
 connect();
@@ -19,4 +19,6 @@ app.get("/crew/:name", getCrew);
 app.get("/technology/:name", getTech);
 app.use("/", ...swaggerMiddleware());
 
-app.listen(3000);
+app.listen(port, "0.0.0.0", function () {
+    console.log("errors")
+  });
