e68ea207c2748623536283880573fa9094fde2d6
Lado Asambadze <lado.asambadze1@gmail.com>
2023-06-14 13:45:07 +0400

    test

--- src/app.js
+++ src/app.js
@@ -7,18 +7,16 @@ import getDest from "./controllers/dest-controller.js";
 import swaggerMiddleware from "./middlewares/swg-midl.js";
 import getCrew from "./controllers/crew-controller.js";
 import getTech from "./controllers/tech-controller.js";
-const port = process.env.MONGO_URL || 3000;
+
 const app = express();
 dotenv.config();
 connect();
 app.use(cors());
 app.use(bodyParser.json());
-
+const port = process.env.MONGO_URL || 3000;
 app.get("/destinations/:name", getDest);
 app.get("/crew/:name", getCrew);
 app.get("/technology/:name", getTech);
 app.use("/", ...swaggerMiddleware());
 
-app.listen(port, "0.0.0.0", function () {
-  console.log("errors");
-});
+app.listen(port);
